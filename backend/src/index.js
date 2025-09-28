59d1958f6a6ce9cbf8c861139565e57f3be8b4c9
Daksh Thakran <daksh.thakran05@gmail.com>
2025-09-28 23:33:04 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,6 +26,7 @@ const corsOptions = {
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://ayurtrace-sepia.vercel.app",
     "https://ayurtrace.onrender.com"
   ],
   credentials: true,
